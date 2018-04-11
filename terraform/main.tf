0f614b31740529d53b9b23ec37fe558f35fba245
Danielle Marie <danielle.m.leboeuf@gmail.com>
2018-04-11 19:24:58 -0400

    testing first main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,81 @@ terraform {
 provider "google" {
   region = "us-central1"
 }
+
+//instances
+
+  resource "google_compute_instance_template" "default" {
+  name        = "appserver-template"
+  description = "This template is used to create app server instances."
+
+  tags = ["foo", "bar"]
+
+  labels = {
+    environment = "dev"
+  }
+
+  instance_description = "description assigned to instances"
+  machine_type         = "n1-standard-1"
+  can_ip_forward       = false
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  // Create a new boot disk from an image
+  disk {
+    source_image = "debian-cloud/debian-8"
+    auto_delete  = true
+    boot         = true
+  }
+
+  // Use an existing disk resource
+  disk {
+    source      = "foo_existing_disk"
+    auto_delete = false
+    boot        = false
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  metadata {
+    foo = "bar"
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+
+
+//manager
+resource "google_compute_instance_template" "instance_template" {
+  name_prefix  = "instance-template-"
+  machine_type = "n1-standard-1"
+  region       = "us-central1"
+
+  // boot disk
+  disk {
+    # ...
+  }
+
+  // networking
+  network_interface {
+    # ...
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_compute_instance_group_manager" "instance_group_manager" {
+  name               = "instance-group-manager"
+  instance_template  = "${google_compute_instance_template.instance_template.self_link}"
+  base_instance_name = "instance-group-manager"
+  zone               = "us-central1-f"
+  target_size        = "1"
+}
