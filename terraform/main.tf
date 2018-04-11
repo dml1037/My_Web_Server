7331d0f7ee069dea994f93bda77d298c11039c0b
Danielle Marie <danielle.m.leboeuf@gmail.com>
2018-04-11 19:54:45 -0400

    pushing 3rd edited file

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,54 +9,6 @@ provider "google" {
   region = "us-central1"
 }
 
-//instances
-
-  resource "google_compute_instance_template" "default" {
-  name        = "appserver-template"
-  description = "This template is used to create app server instances."
-
-  tags = ["foo", "bar"]
-
-  labels = {
-    environment = "dev"
-  }
-
-  instance_description = "description assigned to instances"
-  machine_type         = "n1-standard-1"
-  can_ip_forward       = false
-
-  scheduling {
-    automatic_restart   = true
-    on_host_maintenance = "MIGRATE"
-  }
-
-  // Create a new boot disk from an image
-  disk {
-    source_image = "debian-cloud/debian-8"
-    auto_delete  = true
-    boot         = true
-  }
-
-  // Use an existing disk resource
-  disk {
-    source      = "foo_existing_disk"
-    auto_delete = false
-    boot        = false
-  }
-
-  network_interface {
-    network = "default"
-  }
-
-  metadata {
-    foo = "bar"
-  }
-
-  service_account {
-    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
-  }
-}
-
 
 //manager
 resource "google_compute_instance_template" "instance_template" {
