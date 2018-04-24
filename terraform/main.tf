6ab04b04a523f13d5ed35b2436f53a3a92ba6465
Danielle Marie <danielle.m.leboeuf@gmail.com>
2018-04-23 20:29:15 -0400

    work already!!!

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,13 +6,11 @@ terraform {
  }
 }
 provider "google" {
-
-  project = "comp698-dml1037"
   region = "us-central1"
 }
 
-resource "google_compute_instance_template" "tf-my-web-server" {
-  name = "tf-my-web-server"
+resource "google_compute_instance_template" "tf-server" {
+  name = "tf-server"
   project = "comp698-dml1037"
   disk {
     source_image = "cos-cloud/cos-stable"
@@ -20,20 +18,41 @@ resource "google_compute_instance_template" "tf-my-web-server" {
   machine_type = "n1-standard-1"
   network_interface {
     network = "default"
-    access_config {
-    }
+    access_config {}
   }
 
+  tags = ["http-server"] 
+
+  service_account {
+    scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/devstorage.read_write",
+    ]
+  }
   metadata {
-    gce-container-declaration = <<EOF
-spec:
-  containers:
+      gce-container-declaration = <<EOF
+  spec:
+    containers:
     - image: 'gcr.io/comp698-dml1037/github-dml1037-my_web_server:8df295e7641bfb190ff365b22fc9b24d85066529'
       name: service-container
       stdin: false
       tty: false
-  restartPolicy: Always
+    restartPolicy: Always
 EOF
+  }
+}
+
+resource "google_compute_instance_template" "tf-server2" {
+  name = "tf-server2"
+  project = "comp698-dml1037"
+  disk {
+    source_image = "cos-cloud/cos-stable"
+  }
+  machine_type = "n1-standard-1"
+  network_interface {
+    network = "default"
+    access_config {}
   }
 
   tags = ["http-server"]
@@ -45,16 +64,35 @@ EOF
       "https://www.googleapis.com/auth/devstorage.read_write",
     ]
   }
+  metadata {
+      gce-container-declaration = <<EOF
+  spec:
+    containers:
+    - image: 'gcr.io/comp698-dml1037/github-dml1037-my_web_server:8df295e7641bfb190ff365b22fc9b24d85066529'
+      name: service-container
+      stdin: false
+      tty: false
+    restartPolicy: Always
+EOF
+  }
 }
 
 resource "google_compute_instance_group_manager" "default" {
-  name = "tf-my-web-server-manager"
+  name = "tf-manager"
   project = "comp698-dml1037"
   zone = "us-central1-f"
-  base_instance_name = "app"
-  instance_template  = "${google_compute_instance_template.tf-my-web-server.self_link}"
+  base_instance_name = "prod"
+  instance_template  = "${google_compute_instance_template.tf-server.self_link}"
   target_size = 2
+}
 
+resource "google_compute_instance_group_manager" "default2" {
+  name = "tf-manager2"
+  project = "comp698-dml1037"
+  zone = "us-central1-f"
+  base_instance_name = "staging"
+  instance_template  = "${google_compute_instance_template.tf-server2.self_link}"
+  target_size = 1
 }
 
 resource "google_storage_bucket" "image-store" {
@@ -62,4 +100,3 @@ resource "google_storage_bucket" "image-store" {
   name     = "isunburntooeasily"
   location = "us-central1"
 }
-
